b35b380f5a56619825d65d23619aee51e685dfa0
future.wei <future.wei@technet-asia.com>
2019-05-03 13:39:02 +0800

    Updated

---
+++ Test Suites/MVP Run.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MVP Run</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>d8fdae81-35a5-4947-975b-820c4a96d575</testSuiteGuid>
+   <testCaseLink>
+      <guid>43212483-c536-4f88-a413-f1d4bda2b0a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MVP weather</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
